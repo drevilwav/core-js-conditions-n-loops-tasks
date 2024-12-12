3d61b5aa2eecefdd70febee10cdb0a6cf45d37c5
Vladimir <vl.kutepov@gmail.com>
2024-12-13 00:56:35 +1000

    10/19

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -264,8 +264,15 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let result = -1;
+  for (let i = 0; i < str.length - 1; i += 1) {
+    if (str[i] === letter) {
+      result = i;
+      break;
+    }
+  }
+  return result;
 }
 
 /**
@@ -283,8 +290,15 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const str = `${num}`;
+  const dig = `${digit}`;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === dig) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
